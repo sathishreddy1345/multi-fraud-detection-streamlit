c7de352b6a25ce3a6b7bd62d583670f628138824
SATHISH REDDY <sathishkumarreddy66@gmail.com>
2025-11-17 17:16:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import warnings
+import numpy as np
+import pandas as pd
+
 warnings.filterwarnings("ignore", category=FutureWarning, message=".*force_all_finite.*")
 
 # Fraud modules
